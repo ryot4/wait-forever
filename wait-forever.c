f5842dff215bd71c891bdaf28c3448d41e63afc6
FUJII Ryota <rf@readonly.xyz>
2017-11-18 21:47:36 +0900

    Add ability to write own pid to a file

--- wait-forever.c
+++ wait-forever.c
@@ -1,7 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-int
-main(void)
+void
+writepid(const char *pidfile)
 {
+	FILE *fp = fopen(pidfile, "w");
+	if (fp == NULL) {
+		perror("cannot open pid file");
+		exit(EXIT_FAILURE);
+	}
+	fprintf(fp, "%d\n", getpid());
+	fclose(fp);
+}
+
+int
+main(int argc, char **argv)
+{
+	if (argc > 1) {
+		if (!strcmp(argv[1], "-h")) {
+			printf("%s [pidfile]\n", argv[0]);
+			return 0;
+		} else {
+			writepid(argv[1]);
+		}
+	}
 	pause();
 }
