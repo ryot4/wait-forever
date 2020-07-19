3f32f4643a92505d399ac2d2dfa74b6de159edab
FUJII Ryota <rf@readonly.xyz>
2020-07-19 14:09:21 +0900

    Refactor

--- wait-forever.c
+++ wait-forever.c
@@ -3,6 +3,30 @@
 #include <string.h>
 #include <unistd.h>
 
+void writepid(const char *pidfile);
+
+int
+main(int argc, char **argv)
+{
+	const char *pidfile = NULL;
+
+	for (int i = 1 ; i < argc; ++i) {
+		if (strcmp(argv[i], "-h") == 0) {
+			printf("%s [pidfile]\n", argv[0]);
+			return 0;
+		} else {
+			pidfile = argv[i];
+			break;
+		}
+	}
+
+	if (pidfile != NULL) {
+		writepid(pidfile);
+	}
+
+	pause();
+}
+
 void
 writepid(const char *pidfile)
 {
@@ -13,18 +37,4 @@ writepid(const char *pidfile)
 	}
 	fprintf(fp, "%d\n", getpid());
 	fclose(fp);
-}
-
-int
-main(int argc, char **argv)
-{
-	if (argc > 1) {
-		if (!strcmp(argv[1], "-h")) {
-			printf("%s [pidfile]\n", argv[0]);
-			return 0;
-		} else {
-			writepid(argv[1]);
-		}
-	}
-	pause();
 }
